3666c28c48a703d5a65cccc05f6be1381979051c
log <lrvanpel@asu.edu>
2026-08-04 23:12:21 -0700

    Initial benchmark script

---
+++ bench.py
@@ -0,0 +1,109 @@
+import argparse
+import shlex
+import subprocess
+import sys
+from pathlib import Path
+from typing import Optional
+
+
+def run(cmd: list[str]) -> int:
+    ret = subprocess.run(
+        cmd,
+        check=False,
+    ).returncode
+    return ret
+
+
+def get_current_head() -> str:
+    try:
+        output = subprocess.check_output(
+            ["git", "symbolic-ref", "--short", "HEAD"], text=True
+        )
+    except subprocess.SubprocessError:
+        output = subprocess.check_output(["git", "rev-parse", "HEAD"], text=True)
+    return output.strip()
+
+
+def tree_is_dirty() -> bool:
+    ret = run(["git", "diff-index", "--quiet", "HEAD", "--"])
+    return ret != 0
+
+
+def rev_exists(rev: str) -> bool:
+    ret = run(["git", "rev-parse", "--verify", "--quiet", rev])
+    return ret == 0
+
+
+def checkout(rev: str) -> bool:
+    ret = run(["git", "checkout", rev, "--"])
+    return ret == 0
+
+
+def build(release=True) -> bool:
+    cmd = ["cargo", "build"]
+    if release:
+        cmd.append("--release")
+    ret = run(cmd)
+    return ret == 0
+
+
+def move_artifact(old: Path, new: str) -> Path:
+    return old.rename(new)
+
+
+def benchmark(a: Path, b: Path, args: Optional[str]):
+    a_cmd = str(a) if args is None else " ".join((str(a), args))
+    b_cmd = str(b) if args is None else " ".join((str(b), args))
+    cmd = ["hyperfine", a_cmd, b_cmd]
+    print(cmd)
+    run(cmd)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Cargo benchmarking helper using hyperfine"
+    )
+    parser.add_argument("a", help="git revision A to compare against B")
+    parser.add_argument("b", help="git revision B to compare against A")
+    parser.add_argument(
+        "--args", default=None, help="arguments to pass for the benchmark"
+    )
+    parser.add_argument(
+        "--artifact",
+        type=Path,
+        default=Path("./target/release/sudoku.exe"),
+        help="The binary produced by cargo build to benchmark",
+    )
+    args = parser.parse_args()
+
+    if tree_is_dirty():
+        print("Tree is dirty!")
+        sys.exit(1)
+
+    build_artifact = args.artifact
+    rev_a = args.a
+    rev_b = args.b
+    assert rev_exists(rev_a)
+    assert rev_exists(rev_b)
+
+    prior_state = get_current_head()
+    print(f"Current head: {prior_state}")
+
+    try:
+        print(f"Checking out {rev_a}")
+        assert checkout(rev_a)
+        assert build()
+        artifact_a = move_artifact(build_artifact, "rev_a.exe")
+        print("Built rev_a!")
+
+        print(f"Checking out {rev_b}")
+        assert checkout(rev_b)
+        assert build()
+        artifact_b = move_artifact(build_artifact, "rev_b.exe")
+        print("Built rev_b!")
+
+        benchmark(artifact_a, artifact_b, args.args)
+
+    finally:
+        print(f"Returning to previous HEAD ({prior_state})")
+        checkout(prior_state)
