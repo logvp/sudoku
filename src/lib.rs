23d28a63faba2529956c3950cb91f0ed7da3cbb4
log <lrvanpel@asu.edu>
2026-08-07 21:53:07 -0700

    Add clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+#![deny(
+    unsafe_code,
+    clippy::correctness,
+    clippy::suspicious,
+    unused_must_use,
+    unfulfilled_lint_expectations
+)]
+#![warn(clippy::complexity, clippy::perf, clippy::style)]
+#![warn(clippy::pedantic)]
+#![allow(
+    clippy::missing_panics_doc,
+    clippy::wildcard_imports,
+    clippy::semicolon_if_nothing_returned,
+    clippy::uninlined_format_args,
+    clippy::missing_errors_doc,
+    clippy::match_same_arms,
+    clippy::needless_continue,
+    clippy::ignored_unit_patterns,
+    clippy::must_use_candidate
+)]
+
 mod rules;
 use std::fmt::Display;
 
