ec70ff458face23ab9beeb47877a6b3ffd0a83f1
log <lrvanpel@asu.edu>
2026-08-08 16:34:10 -0700

    Revert "Reorder check"
    This reverts commit 4b5e32ef3ef17140284c380145677b14f7f75e0a.

--- src/optimized.rs
+++ src/optimized.rs
@@ -1,6 +1,6 @@
 use log::debug;
 
-use crate::{Board, Digit, DigitSet, SudokuBox, SudokuColumn, SudokuRow, SudokuRule};
+use crate::{Board, Digit, SudokuBox, SudokuColumn, SudokuRow, SudokuRule};
 
 fn check_board_one(board: &Board, index: usize) -> bool {
     SudokuRow {}.check_one(board, index)
@@ -13,17 +13,12 @@ pub fn solve_standard(board: Board) -> Option<Board> {
         debug!("Board is already solved!");
         return Some(board);
     };
-    let mut possible = DigitSet::new();
     for digit in Digit::DIGITS {
         let mut solution = board.clone();
         solution.board[check_idx] = Some(digit);
-        if check_board_one(&solution, check_idx) {
-            possible.set(digit);
+        if !check_board_one(&solution, check_idx) {
+            continue;
         }
-    }
-    for digit in possible.into_iter() {
-        let mut solution = board.clone();
-        solution.board[check_idx] = Some(digit);
         if let Some(solved) = solve_standard(solution) {
             return Some(solved);
         }
