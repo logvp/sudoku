0c24433eb31a12da94164f3c166c8c47688dcb46
log <lrvanpel@asu.edu>
2026-08-08 20:42:41 -0700

    Nonrecursive solve
    Summary
      rev_b.exe ran
        2.67 ± 0.04 times faster than rev_a.exe

--- src/optimized.rs
+++ src/optimized.rs
@@ -1,4 +1,4 @@
-use log::debug;
+use log::{debug, error};
 
 use crate::{Board, Digit, SudokuBox, SudokuColumn, SudokuRow, SudokuRule};
 
@@ -8,21 +8,53 @@ fn check_board_one(board: &Board, index: usize) -> bool {
         && SudokuBox {}.check_one(board, index)
 }
 
-pub fn solve_standard(board: Board) -> Option<Board> {
-    let Some(check_idx) = board.first_open_index() else {
+fn check_board_all(board: &Board) -> bool {
+    SudokuRow {}.check(board) && SudokuColumn {}.check(board) && SudokuBox {}.check(board)
+}
+
+pub fn solve_standard(mut board: Board) -> Option<Board> {
+    if !check_board_all(&board) {
+        error!("Board is unsolvable");
+        return None;
+    }
+
+    let mut stack: Vec<usize> = Vec::new();
+    if let Some(check_idx) = board.first_open_index() {
+        stack.push(check_idx);
+        board.board[check_idx] = Some(Digit::_1);
+    } else {
         debug!("Board is already solved!");
         return Some(board);
     };
-    for digit in Digit::DIGITS {
-        let mut solution = board.clone();
-        solution.board[check_idx] = Some(digit);
-        if !check_board_one(&solution, check_idx) {
-            continue;
-        }
-        if let Some(solved) = solve_standard(solution) {
-            return Some(solved);
+
+    let mut must_backtrack = false;
+    while !stack.is_empty() {
+        // if the guess was valid, continue on to the next open spot
+        let check_idx = stack
+            .last()
+            .copied()
+            .expect("unreachable because stack is not empty");
+        if !must_backtrack && check_board_one(&board, check_idx) {
+            let Some(next_open) = board.first_open_index() else {
+                return Some(board);
+            };
+            stack.push(next_open);
+            board.board[next_open] = Some(Digit::_1);
+        } else {
+            must_backtrack = false;
+            // if the guess was invalid, increment the guess
+            if let Some(next_digit) = board.board[check_idx].unwrap().next() {
+                board.board[check_idx] = Some(next_digit);
+            }
+            // if we exhausted all guesses for this index, rewind guess and backtrack
+            else {
+                board.board[check_idx] = None;
+                stack.pop().expect("unreachable because stack is not empty");
+                must_backtrack = true;
+            }
         }
     }
+
     None
 }
 
@@ -56,6 +88,10 @@ mod tests {
             1, 3, 2, 8, 4, 6, 5, 7, 9,
             4, 6, 7, 5, 9, 3, 8, 2, 1,
         ]);
-        assert_eq!(solve_standard(board), Some(solution))
+        println!("input:");
+        board.print();
+        let answer = solve_standard(board).unwrap();
+        answer.print();
+        assert_eq!(answer, solution)
     }
 }
