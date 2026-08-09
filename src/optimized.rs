53bf2d1df61d30fa27697ff27600ce7f3c67cbc1
log <lrvanpel@asu.edu>
2026-08-08 19:30:43 -0700

    Revert "Use DigitSet in optimized solver"
    This reverts commit d2f44624ebc0e4df4af8b2c8303d7a675b8a5d72.

--- src/optimized.rs
+++ src/optimized.rs
@@ -1,37 +1,24 @@
 use log::debug;
 
-use crate::{Board, DigitSet};
+use crate::{Board, Digit, SudokuBox, SudokuColumn, SudokuRow, SudokuRule};
+
+fn check_board_one(board: &Board, index: usize) -> bool {
+    SudokuRow {}.check_one(board, index)
+        && SudokuColumn {}.check_one(board, index)
+        && SudokuBox {}.check_one(board, index)
+}
 
 pub fn solve_standard(board: Board) -> Option<Board> {
-    let Some(empty_idx) = board.first_open_index() else {
+    let Some(check_idx) = board.first_open_index() else {
         debug!("Board is already solved!");
         return Some(board);
     };
-    let mut seen = DigitSet::new();
-    let (x, y) = board.xy(empty_idx);
-    for i in 0..board.width() {
-        if let Some(digit) = board.get(i, y) {
-            seen.set(digit);
-        }
-    }
-    for j in 0..board.height() {
-        if let Some(digit) = board.get(x, j) {
-            seen.set(digit);
-        }
-    }
-    let box_start_x = x - (x % 3);
-    let box_start_y = y - (y % 3);
-    for i in box_start_x..(box_start_x + 3) {
-        for j in box_start_y..(box_start_y + 3) {
-            if let Some(digit) = board.get(i, j) {
-                seen.set(digit);
-            }
-        }
-    }
-    seen.invert();
-    for digit in seen {
+    for digit in Digit::DIGITS {
         let mut solution = board.clone();
-        solution.board[empty_idx] = Some(digit);
+        solution.board[check_idx] = Some(digit);
+        if !check_board_one(&solution, check_idx) {
+            continue;
+        }
         if let Some(solved) = solve_standard(solution) {
             return Some(solved);
         }
