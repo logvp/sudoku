85557fe1fe089e3eae142f6f0c37d8bb1c1168e8
log <lrvanpel@asu.edu>
2026-09-03 23:32:29 -0700

    Lokjge check

--- examples/check.rs
+++ examples/check.rs
@@ -18,18 +18,24 @@ fn main() {
     let mut proper = 0;
     let mut ambiguous = 0;
     let mut unsolvable = 0;
-    for i in 0..board.len() {
-        let mut this_board = board.clone();
+    for j in 0..board.len() {
+        let mut board = board.clone();
+        let (x, y) = board.xy(j);
+        board.reset(x, y);
 
-        let (x, y) = this_board.xy(i);
-        let digit = this_board.get(x, y).unwrap_or(Digit::_9);
-        this_board.set(x, y, digit.shift());
+        for i in 0..board.len() {
+            let mut this_board = board.clone();
 
-        match verify(this_board, None) {
-            BoardStatus::AlreadySolved => already_solved += 1,
-            BoardStatus::OneSolution => proper += 1,
-            BoardStatus::MultipleSolutions => ambiguous += 1,
-            BoardStatus::Unsolvable => unsolvable += 1,
+            let (x, y) = this_board.xy(i);
+            let digit = this_board.get(x, y).unwrap_or(Digit::_9);
+            this_board.set(x, y, digit.shift());
+
+            match verify(this_board, None) {
+                BoardStatus::AlreadySolved => already_solved += 1,
+                BoardStatus::OneSolution => proper += 1,
+                BoardStatus::MultipleSolutions => ambiguous += 1,
+                BoardStatus::Unsolvable => unsolvable += 1,
+            }
         }
     }
     println!("Done!");
