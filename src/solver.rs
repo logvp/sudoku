71ab9575a85f3fddbf48760f2b02aaeee0727a56
log <lrvanpel@asu.edu>
2026-09-13 14:43:01 -0700

    Move solver tests into solver

--- src/solver.rs
+++ src/solver.rs
@@ -24,3 +24,117 @@ pub enum Action {
     AlreadySolved,
     Abort,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::standard_sudoku_rules;
+
+    fn test_solver_harness<T: Solver>(mut solver: T) {
+        #[rustfmt::skip]
+        let mut board: Board = Board::make([
+            0, 0, 0, 2, 0, 9, 0, 0, 0,
+            9, 7, 6, 0, 0, 0, 2, 0, 5,
+            0, 0, 5, 6, 7, 0, 1, 0, 8,
+            0, 8, 0, 9, 0, 0, 0, 0, 7,
+            7, 0, 0, 4, 3, 8, 0, 0, 2,
+            6, 0, 0, 0, 0, 7, 0, 8, 0,
+            5, 0, 8, 0, 1, 2, 3, 0, 0,
+            1, 0, 2, 0, 0, 0, 5, 7, 9,
+            0, 0, 0, 5, 0, 3, 0, 0, 0,
+        ]);
+        let rules = Arbiter::new(standard_sudoku_rules());
+        #[rustfmt::skip]
+        let solution: Board = Board::make([
+            8, 4, 1, 2, 5, 9, 7, 3, 6,
+            9, 7, 6, 3, 8, 1, 2, 4, 5,
+            3, 2, 5, 6, 7, 4, 1, 9, 8,
+            2, 8, 3, 9, 6, 5, 4, 1, 7,
+            7, 1, 9, 4, 3, 8, 6, 5, 2,
+            6, 5, 4, 1, 2, 7, 9, 8, 3,
+            5, 9, 8, 7, 1, 2, 3, 6, 4,
+            1, 3, 2, 8, 4, 6, 5, 7, 9,
+            4, 6, 7, 5, 9, 3, 8, 2, 1,
+        ]);
+        loop {
+            let action = solver.make_move(&board, &rules);
+            assert!(rules.update(&mut board, action).is_ok());
+            assert!(rules.check(&board));
+            if rules.is_solved(&board) {
+                break;
+            }
+        }
+        assert_eq!(board, solution);
+    }
+
+    #[test]
+    fn test_backtracking_solver() {
+        let solver = BacktrackingSolver::default();
+        test_solver_harness(solver);
+    }
+
+    #[test]
+    fn test_constraint_solver() {
+        let solver = ConstraintSolver::default();
+        test_solver_harness(solver);
+    }
+
+    #[test]
+    fn test_verify_board() {
+        #[rustfmt::skip]
+        let board: Board = Board::make([
+            0, 0, 0, 2, 0, 9, 0, 0, 0,
+            9, 7, 6, 0, 0, 0, 2, 0, 5,
+            0, 0, 5, 6, 7, 0, 1, 0, 8,
+            0, 8, 0, 9, 0, 0, 0, 0, 7,
+            7, 0, 0, 4, 3, 8, 0, 0, 2,
+            6, 0, 0, 0, 0, 7, 0, 8, 0,
+            5, 0, 8, 0, 1, 2, 3, 0, 0,
+            1, 0, 2, 0, 0, 0, 5, 7, 9,
+            0, 0, 0, 5, 0, 3, 0, 0, 0,
+        ]);
+        assert_eq!(verify(board, None), BoardStatus::OneSolution);
+
+        #[rustfmt::skip]
+        let board: Board = Board::make([
+            8, 4, 1, 2, 5, 9, 7, 3, 6,
+            9, 7, 6, 3, 8, 1, 2, 4, 5,
+            3, 2, 5, 6, 7, 4, 1, 9, 8,
+            2, 8, 3, 9, 6, 5, 4, 1, 7,
+            7, 1, 9, 4, 3, 8, 6, 5, 2,
+            6, 5, 4, 1, 2, 7, 9, 8, 3,
+            5, 9, 8, 7, 1, 2, 3, 6, 4,
+            1, 3, 2, 8, 4, 6, 5, 7, 9,
+            4, 6, 7, 5, 9, 3, 8, 2, 1,
+        ]);
+        assert_eq!(verify(board, None), BoardStatus::AlreadySolved);
+
+        // #[rustfmt::skip]
+        // let board: Board = Board::make([
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //     0, 0, 0, 0, 0, 0, 0, 0, 0,
+        // ]);
+        // assert_eq!(verify(board, None), BoardStatus::MultipleSolutions);
+
+        #[rustfmt::skip]
+        let board: Board = Board::make([
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ]);
+        assert_eq!(verify(board, None), BoardStatus::Unsolvable);
+    }
+}
